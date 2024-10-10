676ef155730935a4ee01961ebbe2f60d58f296a0
bryntam <112902843+bryntam@users.noreply.github.com>
2024-10-10 14:38:01 -0400

    basic code

--- timer.py
+++ timer.py
@@ -8,11 +8,23 @@ timer.py uses the time library to help keep track of time
 
 # This program is timer that counts down
 
-
+import random # The random library will be used to generate random numbers
 import time # The time library has a sleep function that will pause the script for a specifized amount of time
 from PIL import Image # the pillow library makes it easy to display images 
 
 im = Image.open("times-up.jpeg")
+
+def nerveOfSteelGame():
+    
+    print("Players stand")
+
+    #list of players sitting
+    players_sitting = []
+
+    random_time = random.randint(10, 25)
+
+    start_time = time.time()
+
 
 # ask user to enter desired countdown time
 set_time = int(input("Please set your timer in seconds: "))
